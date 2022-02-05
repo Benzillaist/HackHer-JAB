db8316d655f555be7f049d88a1b0fc651f4a6376
Benzillaist <benkuchma@gmail.com>
2022-02-05 15:32:48 -0500

    background

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
-import sys, pygame
+from locale import CHAR_MAX
+import sys, pygame, math
 
 pygame.init()
 
@@ -11,7 +12,14 @@ screen = pygame.display.set_mode(size)
 ball = pygame.image.load("dvd.png")
 ballrect = ball.get_rect()
 
+wall = pygame.image.load("tilefloor.jpg")
+wallrect = wall.get_rect()
+
 count = 0
+char_X = 0
+char_Y = 0
+
+sqrt2 = math.sqrt(2)
 
 while 1:
     for event in pygame.event.get():
@@ -20,24 +28,32 @@ while 1:
     
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_a]:
-        print("Testa")
-        if ballrect.left > 0:
+    if keys[pygame.K_a] and keys[pygame.K_w]:
+        if ballrect.left > 0 and ballrect.top > 0:
+            ballrect.move_ip(-1, -1)
+        elif ballrect.left > 0:
             ballrect.move_ip(-1, 0)
-    if keys[pygame.K_w]:
-        print("Tests")
-        if ballrect.top > 0:
+        elif ballrect.top > 0:
             ballrect.move_ip(0, -1)
+    #if keys[pygame.K_a]:
+    #    if ballrect.left > 0:
+    #        ballrect.move_ip(-1, 0)
+    #if keys[pygame.K_w]:
+    #    if ballrect.top > 0:
+    #        ballrect.move_ip(0, -1)
     if keys[pygame.K_d]:
-        print("Testd")
         if ballrect.right < width:
             ballrect.move_ip(1, 0)
     if keys[pygame.K_s]:
-        print("Tests")
         if ballrect.bottom < height:
             ballrect.move_ip(0, 1)
 
+    for y in range(0, height, 400):
+        for x in range(0, width, 400):
+            screen.blit(wall, (x , y))
+            #print(f'width: {x} height: {y}')
+    
+    #screen.fill(black)
 
-    screen.fill(black)
     screen.blit(ball, ballrect)
     pygame.display.flip()
