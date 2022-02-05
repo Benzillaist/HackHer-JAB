b38313774e9ef5cfb3171a2dbf644b62d2a41b19
Benzillaist <benkuchma@gmail.com>
2022-02-05 13:33:47 -0500

    dvd

--- testing.py
+++ testing.py
@@ -2,25 +2,41 @@ import sys, pygame
 
 pygame.init()
 
-size = width, height = 320, 240
+size = width, height = 1000, 600
 speed = [1, 1]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("john.jpg")
+ball = pygame.image.load("dvd.png")
 ballrect = ball.get_rect()
+
+count = 0
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_a]:
+        print("Testa")
+        if ballrect.left > 0:
+            ballrect.move_ip(-1, 0)
+    if keys[pygame.K_w]:
+        print("Tests")
+        if ballrect.top > 0:
+            ballrect.move_ip(0, -1)
+    if keys[pygame.K_d]:
+        print("Testd")
+        if ballrect.right < width:
+            ballrect.move_ip(1, 0)
+    if keys[pygame.K_s]:
+        print("Tests")
+        if ballrect.bottom < height:
+            ballrect.move_ip(0, 1)
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
 
     screen.fill(black)
     screen.blit(ball, ballrect)
