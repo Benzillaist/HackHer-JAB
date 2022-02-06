86d3e8f172c02870a8ff06aaea43eb257f2a22cb
Benzillaist <benkuchma@gmail.com>
2022-02-05 19:18:16 -0500

    map

--- testing.py
+++ testing.py
@@ -1,9 +1,10 @@
+from concurrent.futures import wait
 from locale import CHAR_MAX
-import sys, pygame, math
+import sys, pygame, time
 
 pygame.init()
 
-size = width, height = 1000, 600
+size = width, height = 1000, 1000
 speed = [1, 1]
 black = 0, 0, 0
 
@@ -14,47 +15,54 @@ cameraBoarder = 50
 bound_X = 1000
 bound_Y = 1000
 
+count = 0
+
 screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("dvd.png")
 ballrect = ball.get_rect()
 ballrect.move_ip(cameraBoarder, cameraBoarder)
 
-wall = pygame.image.load("tilefloor.jpg")
+wall = pygame.image.load("office2000.png")
+#wall = pygame.transform.scale(wall, (20, 20))
 wallrect = wall.get_rect()
 
+screen.blit(wall, (0,0))
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
+    #time.sleep(0.001)
+
     keys = pygame.key.get_pressed()
     
     if keys[pygame.K_a]:
         if ballrect.left > cameraBoarder:
-            ballrect.move_ip(-1, 0)
+            ballrect.move_ip(-5, 0)
         elif cameraOff_X > cameraBoarder:
             cameraOff_X -= 1
-    if keys[pygame.K_w]:
-        if ballrect.top > cameraBoarder:
-            ballrect.move_ip(0, -1)
-        elif cameraOff_Y > cameraBoarder:
-            cameraOff_Y -= 1
-    if keys[pygame.K_d]:
+    elif keys[pygame.K_d]:
         if ballrect.right < width - cameraBoarder:
-            ballrect.move_ip(1, 0)
+            ballrect.move_ip(5, 0)
         elif cameraOff_X < bound_X - cameraBoarder:
             cameraOff_X += 1
-    if keys[pygame.K_s]:
+    if keys[pygame.K_w]:
+        if ballrect.top > cameraBoarder:
+            ballrect.move_ip(0, -5)
+        elif cameraOff_Y > cameraBoarder:
+            cameraOff_Y -= 1
+    elif keys[pygame.K_s]:
         if ballrect.bottom <= height - cameraBoarder:
-            ballrect.move_ip(0, 1)
+            ballrect.move_ip(0, 5)
         elif cameraOff_Y < bound_Y - cameraBoarder:
             cameraOff_Y += 1
 
-    for y in range(0, bound_Y + height, ball.get_height()):
-        for x in range(0, bound_X + width, ball.get_width()):
-            screen.blit(wall, (x - cameraOff_X, y - cameraOff_Y))
+    #wall.scroll(-cameraOff_X, -cameraOff_Y)
+    screen.blit(wall, (-cameraOff_X, -cameraOff_Y))
+    
 
+    screen.blit
     screen.blit(ball, ballrect)
     pygame.display.flip()
