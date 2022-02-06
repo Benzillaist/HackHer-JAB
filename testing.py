83f203d155744c9ff4365b9ab47c5b7af9734cb4
agnesrohra <61053622+agnesrohra@users.noreply.github.com>
2022-02-05 22:46:48 -0500

    Adding Second Scene

--- testing.py
+++ testing.py
@@ -7,6 +7,9 @@ user32 = ctypes.windll.user32
 screen_X = user32.GetSystemMetrics(0)
 screen_Y = user32.GetSystemMetrics(1)
 
+printerVar = 0
+computerVar = 0
+sexualHarassment = 0
 
 pygame.init()
 
@@ -50,6 +53,14 @@ d2 = font.render("My name is Cathy.", True, (0,0,0))
 textRect2 = d2.get_rect()
 textRect2.center = (450,550)
 
+d3 = font.render("You have certain tasks to complete in each work day.", True, (0,0,0))
+textRect3 = d3.get_rect()
+textRect3.center = (450,550)
+
+d4 = font.render("Your first task is printing documents using the printer at the top right of the map.", True, (0,0,0))
+textRect4 = d4.get_rect()
+textRect4.center = (450,550)
+
 numPressed = 0
 #bg = pygame.image.load("office2000.png")
 
@@ -58,6 +69,41 @@ def dialogue():
         screen.blit(d1, (textRect))
     elif numPressed == 1:
         screen.blit(d2, (textRect2))
+    elif numPressed == 2:
+        screen.blit(d3, (textRect3))
+    elif numPressed == 3:
+        screen.blit(d4, (textRect4))
+    elif numPressed == 4:
+        screen.blit(e2, rect2)
+
+# DIALOGUE D1S2
+
+    #In proximity of printer
+
+    e1 = font.render("Press P to print your documents", True, (0,0,0))
+    rect = e1.get_rect()
+    rect.center = (450,550)
+
+def printerTask():
+    screen.blit(e1, rect)
+
+    #After a delay of 2 seconds - maybe loading bar?
+
+    e2 = font.render("And you're done!", True, (0,0,0))
+    rect2 = e2.get_rect()
+    rect2.center = (450,550)
+
+    #Ernest walks up and starts talking
+
+    e3 = font.render("Ernest: Hey there! Are you the newbie?", True, (0,0,0))
+    rect3 = e3.get_rect()
+    rect3.center = (450,550)
+
+    e4 = font.render("Ernest: I'm the branch manager and your boss, Ernest", True, (0,0,0))
+    rect4 = e4.get_rect()
+    rect4.center = (450,550)
+
+
 
 # GAME LOOP
 
@@ -91,8 +137,11 @@ while 1:
         elif cameraOff_Y < bound_Y - cameraBorder:
             cameraOff_Y += 5
     if keys[pygame.K_SPACE]:
-        numPressed+=1
-        if(numPressed<2):
+        if numPressed < 3:
+            numPressed += 1
+            time.sleep(0.5)
+        if numPressed >= 3 and numPressed < 8 and printerTask == 1:
+            numPressed+=1
             time.sleep(0.5)
     print(ballrect.center) 
     print(f'{cameraOff_X}, {cameraOff_Y}')
@@ -107,5 +156,6 @@ while 1:
     if math.abs(ballrect.centerx - 657+1040) < 100 and math.abs(ballrect.centery - 331) < 100): 
         printerTask()
         if keys[pygame.K_p]:
-            #dialogue2() 
+            printerTask = 1
+            dialogue()
     """ 
